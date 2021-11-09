16a0ca2ef4db1988204fd3f7f0897e26e6e7b39b
Mehmet <mehmetk6158@gmail.com>
2021-11-08 20:49:33 -0500

    2. Convert an array of hashes into

--- converting_data_practice_problems.rb
+++ converting_data_practice_problems.rb
@@ -1,9 +1,27 @@
 #  1. Convert an array of arrays into a hash. For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
-arr = [[1, 3], [8, 9], [2, 16]];
-hash = {}
-arr.each do |item|
-  hash[item[0]] = item[1]
 
+
+# arr = [[1, 3], [8, 9], [2, 16]];
+# hash = {}
+# arr.each do |item|
+#   hash[item[0]] = item[1]
+# end
+# p hash
+
+
+
+#  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+#     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+arr = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+ hash = {}
+
+arr.each do |item|
+  hash[item[:id]] = item
 end
+
 p hash
+
+
+
