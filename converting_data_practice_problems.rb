4d23ce285362870c8d085fdf02522f6c29408644
Mehmet <mehmetk6158@gmail.com>
2021-11-13 23:38:15 -0500

    exercise 5

--- converting_data_practice_problems.rb
+++ converting_data_practice_problems.rb
@@ -20,22 +20,35 @@
 # end
 # p hash
 
+
 # 3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
  #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+  # chars = "bookkeeper".chars
+  # p chars
+  # hash_chars = {}
 
-  chars = "bookkeeper".chars
-  p chars
-  hash_chars = {}
+  # chars.each do |char|
+  #   if hash_chars[char]
+  #     hash_chars[char] += 1
+  #   else
+  #     hash_chars[char] = 1
+  #   end
+  # end
+  # p hash_chars
 
-  chars.each do |char|
-    if hash_chars[char]
-      hash_chars[char] += 1
-    else
-      hash_chars[char] = 1
-    end
+
+  # 5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+  # For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+  info_hash = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+
+  arr = []
+
+  info_hash.each do |k,v|
+    v[:id] = k
+    arr << v
   end
-
-  p hash_chars
+  p arr
 
 
 
