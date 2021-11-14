60320ace2091e13c659a4b9d0d6580af03a2f7dd
Mehmet <mehmetk6158@gmail.com>
2021-11-13 23:17:00 -0500

    exercise-3

--- converting_data_practice_problems.rb
+++ converting_data_practice_problems.rb
@@ -13,15 +13,29 @@
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+# arr = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+#  hash = {}
+# arr.each do |item|
+#   hash[item[:id]] = item
+# end
+# p hash
 
-arr = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
- hash = {}
+# 3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+ #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
 
-arr.each do |item|
-  hash[item[:id]] = item
-end
+  chars = "bookkeeper".chars
+  p chars
+  hash_chars = {}
 
-p hash
+  chars.each do |char|
+    if hash_chars[char]
+      hash_chars[char] += 1
+    else
+      hash_chars[char] = 1
+    end
+  end
+
+  p hash_chars
 
 
 
